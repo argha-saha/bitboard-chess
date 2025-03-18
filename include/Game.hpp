f328fe1220ad30a2aef14682f7cf17e0b667c66f
argha-saha <saha56@purdue.edu>
2025-03-17 22:53:09 -0700

    Created chessboard graphics

---
+++ include/Game.hpp
@@ -0,0 +1,29 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+class Game {
+public:
+    Game();
+    bool init();
+    void run();
+    void cleanup();
+    ~Game();
+
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    bool isRunning;
+    static const int WINDOW_SIZE = 640;
+    static const int BOARD_SIZE = 560;
+    static const int SQUARE_SIZE = BOARD_SIZE / 8;
+    static const int BORDER_SIZE = (WINDOW_SIZE - BOARD_SIZE) / 2;
+    static const int BOARD_OUTLINE_THICKNESS = 2;
+
+    void handleEvents();
+    void render();
+};
+
+#endif // GAME_HPP 
---
+++ src/Game.cpp
@@ -0,0 +1,106 @@
+#include "Game.hpp"
+
+Game::Game() : window(nullptr), renderer(nullptr), isRunning(false) {}
+
+bool Game::init() {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_APPLICATION, 
+            "Couldn't initialize SDL: %s", 
+            SDL_GetError()
+        );
+        return false;
+    }
+
+    if (!SDL_CreateWindowAndRenderer(
+        "Chess", 
+        WINDOW_SIZE, 
+        WINDOW_SIZE, 
+        SDL_WINDOW_RESIZABLE, 
+        &window, 
+        &renderer
+    )) {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_APPLICATION, 
+            "Couldn't create window and renderer: %s", 
+            SDL_GetError()
+        );
+
+        return false;
+    }
+
+    isRunning = true;
+    return true;
+}
+
+void Game::handleEvents() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    if (event.type == SDL_EVENT_QUIT) {
+        isRunning = false;
+    }
+}
+
+void Game::render() {
+    SDL_SetRenderDrawColor(renderer, 35, 40, 50, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw the chess board
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            SDL_FRect tile = {
+                static_cast<float>(BORDER_SIZE + col * SQUARE_SIZE),
+                static_cast<float>(BORDER_SIZE + row * SQUARE_SIZE),
+                static_cast<float>(SQUARE_SIZE),
+                static_cast<float>(SQUARE_SIZE)
+            };
+
+            // Alternate between light and dark squares
+            if ((row + col) % 2 == 0) {
+                SDL_SetRenderDrawColor(renderer, 125, 155, 220, 255);
+            } else {
+                SDL_SetRenderDrawColor(renderer, 35, 40, 50, 255);
+            }
+
+            SDL_RenderFillRect(renderer, &tile);
+        }
+    }
+
+    // Draw the border
+    SDL_SetRenderDrawColor(renderer, 125, 155, 220, 255);
+    SDL_FRect boardOutline = {
+        static_cast<float>(BORDER_SIZE),
+        static_cast<float>(BORDER_SIZE),
+        static_cast<float>(BOARD_SIZE),
+        static_cast<float>(BOARD_SIZE)
+    };
+    SDL_RenderRect(renderer, &boardOutline);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::run() {
+    while (isRunning) {
+        handleEvents();
+        render();
+    }
+}
+
+void Game::cleanup() {
+    if (renderer) {
+        SDL_DestroyRenderer(renderer);
+        renderer = nullptr;
+    }
+
+    if (window) {
+        SDL_DestroyWindow(window);
+        window = nullptr;
+    }
+
+    SDL_Quit();
+} 
+
+Game::~Game() {
+    cleanup();
+}
