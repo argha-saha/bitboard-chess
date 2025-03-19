98407c88ad7813e9e78a85409d3b16e29e13aa93
argha-saha <saha56@purdue.edu>
2025-03-18 23:04:37 -0700

    Wrote board constructor

--- include/Board.h
+++ include/Board.h
@@ -2,13 +2,51 @@
 #define CHESS_BOARD_H
 
 #include <vector>
+#include "Chess.h"
 #include "Piece.h"
+
+typedef std::uint64_t U64;
 
 class Board {
 private:
-    int numRows = 0;
-    int numCols = 0;
-    std::vector<std::vector<Piece *>> board;
+    // White piece bitboards
+    U64 whitePawns;
+    U64 whiteKnights;
+    U64 whiteBishops;
+    U64 whiteRooks;
+    U64 whiteQueens;
+    U64 whiteKing;
+
+    // Black piece bitboards
+    U64 blackPawns;
+    U64 blackKnights;
+    U64 blackBishops;
+    U64 blackRooks;
+    U64 blackQueens;
+    U64 blackKing;
+
+    // Castling
+    bool whiteKingMoved;
+    bool whiteKingSideRookMoved;
+    bool whiteQueenSideRookMoved;
+    bool blackKingMoved;
+    bool blackKingSideRookMoved;
+    bool blackQueenSideRookMoved;
+
+    // TODO: Add en passant logic
+
+    // Turn
+    Color turn;
+
+public:
+    Board();
+    void initBoard();
+    U64 getOccupancy() const;
+    bool isTileEmpty() const;
+    bool isWhite() const;
+
+    static int getRank();
+    static int getFile();
 };
 
 #endif // CHESS_BOARD_H
---
+++ include/Game.h
@@ -0,0 +1,11 @@
+#ifndef CHESS_GAME_H
+#define CHESS_GAME_H
+
+#include "Chess.h"
+
+class Game {
+public:
+    Color getOppositeColor();
+};
+
+#endif //CHESS_GAME_H
